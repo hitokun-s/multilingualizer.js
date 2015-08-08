c7d2d0d6dee013c6d56f05c36731f70e1c9078f5
Hitoshi Wada <jgpuauno@gmail.com>
2015-08-08 20:42:45 +0400

    hold selected lang across pages via localStorage

--- multilingualizer.js
+++ multilingualizer.js
@@ -1,11 +1,29 @@
 var DEAFULT_LANG = "en";
 var LANG_REG_EXP = /\[\[([a-z]{2})\]\]([^\[]+)/g;
-
+var isStorageEnabled = ! (typeof localStorage == 'undefined');
 var user_lang = (navigator.userLanguage||navigator.browserLanguage||navigator.language||DEAFULT_LANG).substr(0,2);
 
 var getLangParam = function(){
     var arr = /lang=([a-z]{2})/g.exec(location.search);
     return arr ? arr[1] : null;
+}
+
+var getLangFromStorage = function(){
+    return isStorageEnabled ? localStorage.getItem('lang') : undefined;
+}
+
+var setLang = function(lang){
+    user_lang = lang;
+    if(isStorageEnabled){
+        localStorage.setItem('lang', user_lang);
+    }
+    applyLang();
+}
+
+var applyLang = function(){
+    globalDict.forEach(function(v){
+        $(v.elm).text(v.dict[user_lang]);
+    });
 }
 
 function textNodesUnder(el){
@@ -18,7 +36,11 @@ var globalDict = [];
 
 $(function(){
     
-    user_lang = getLangParam() || user_lang;
+    user_lang = getLangParam() || getLangFromStorage() || user_lang;
+
+    if(isStorageEnabled){
+        localStorage.setItem('lang', user_lang);
+    }
     
     textNodesUnder(document).filter(function(v){
         return LANG_REG_EXP.test(v.nodeValue);
@@ -30,10 +52,7 @@ $(function(){
         }
         globalDict.push({elm:$(v).parent()[0], dict:dict});        
     });
-    
-    globalDict.forEach(function(v){
-        $(v.elm).text(v.dict[user_lang]);
-    });
+    applyLang();
 });
 
 
